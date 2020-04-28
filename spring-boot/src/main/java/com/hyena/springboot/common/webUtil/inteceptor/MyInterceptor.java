32c28ba359d33a38aa0abd8ac8661f8590c3b8e5
yangxingdong <yangxingdong@jd.com>
2020-04-28 15:34:00 +0800

    fix 项目拼写错误

--- spring-boot/src/main/java/com/hyena/springboot/common/webUtil/inteceptor/MyInterceptor.java
+++ spring-boot/src/main/java/com/hyena/springboot/common/webUtil/inteceptor/MyInterceptor.java
@@ -35,4 +35,47 @@ public class MyInterceptor implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 
     }
+
+
+    public static void main(String[] args) throws InterruptedException, ClassNotFoundException {
+        Object lock = new Object();
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    for (int i = 0; i < 10; i++) {
+//                        if(!Thread.currentThread().isInterrupted()) {Thread.currentThread().sleep(1000);}
+//                        System.out.println(i+1+"s");
+//                        if (i == 5) {
+//                            Thread.currentThread().interrupt();
+//                        }
+//                    }
+//                } catch (InterruptedException e) {
+//                    System.out.println(Thread.currentThread().isInterrupted());
+//                    synchronized (lock) {
+//                        try {
+//                            lock.wait();
+//                            System.out.println("----waited");
+//                        } catch (InterruptedException ex) {
+//                            ex.printStackTrace();
+//                        }
+//                    }
+//                }
+//
+//            }
+//        }).start();
+
+//        Thread.currentThread().sleep(10000);
+
+        /**
+         * Unsafe.getUnsafe() 只允许"直接调用方" 的类加载器为bootstrap加载器
+         *
+         * A -> B -> C(Reflection.getCallerClass() = A   but not B)
+         */
+
+    }
+
+
+
 }
--- spring-boot/src/test/java/com/hyena/springboot/service/ServiceTest.java
+++ spring-boot/src/test/java/com/hyena/springboot/service/ServiceTest.java
@@ -1,8 +1,12 @@
 package com.hyena.springboot.service;
 
+import com.hyena.model.User;
 import com.hyena.springboot.BaseTest;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
+
+import java.lang.ref.WeakReference;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Log4j2
 public class ServiceTest extends BaseTest {
@@ -10,5 +14,60 @@ public class ServiceTest extends BaseTest {
 
     @Test
     public void test() {
+
+        User user = new User();
+        WeakReference<User> weakReference = new WeakReference<>(user);
+
+        System.out.println(weakReference.get()==null);
+
+        System.gc();
+
+        System.out.println(weakReference.get()==null);
+
+        ThreadLocal<String> threadLocal = new ThreadLocal<>();
+        threadLocal.set("hello world");
+        String s = threadLocal.get();
+        System.out.println(s);
+
+        System.gc();
+
+        String p = threadLocal.get();
+        System.out.println(p);
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+        AtomicInteger atomic = new AtomicInteger();
+        atomic.incrementAndGet();
+
+        new Thread(() ->
+        {
+            for (int i = 0; i < 100; i++) {
+                try {
+                    Thread.currentThread().sleep(1000);
+                    System.out.println(atomic.incrementAndGet() +  "  " + Thread.currentThread().getName());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+
+        new Thread(() ->
+        {
+            for (int i = 0; i < 100; i++) {
+                try {
+                    Thread.currentThread().sleep(1000);
+                    System.out.println(atomic.incrementAndGet() +  "  " + Thread.currentThread().getName());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
     }
 }
