c158587a36827a3d0d1e44370b643b83d9db31dc
yangxingdong <yangxingdong@jd.com>
2020-04-28 15:42:00 +0800

    添加spring-util模块

---
+++ spring-util/src/main/java/com/hyena/springutil/SpringUtilApplication.java
@@ -0,0 +1,13 @@
+package com.hyena.springutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUtilApplication.class, args);
+    }
+
+}
---
+++ spring-util/src/test/java/com/hyena/springutil/SpringUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyena.springutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
